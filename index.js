a25cfeab81729b4e8fd90c67885bddd9739f3142
ll-nick <github@le-large.net>
2023-07-21 14:47:43 +0200

    Styled buttons. Changing title.

--- index.js
+++ index.js
@@ -38,12 +38,20 @@
         .appendChild(legend.node())
 
 
+    d3.selectAll('#dataset-selectors>button').classed('active', false);
+    d3.select('#dataset-selectors>#movies').classed('active', true);
+
     document.getElementById("kickstarter").addEventListener("click", async () => {
         const url = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json'
         const fetchData = fetch(url).then(response => response.json())
         const data = await fetchData;
         plotter.update(data, svg)
         legendPlotter.update(data, legend)
+        d3.select('#title').text('Kickstarter Pledges')
+        d3.select('#description').text('Top 100 Most Pledged Kickstarter Campaigns Grouped By Category')
+
+        d3.selectAll('#dataset-selectors>button').classed('active', false);
+        d3.select('#dataset-selectors>#kickstarter').classed('active', true);
     });
 
     document.getElementById("movies").addEventListener("click", async () => {
@@ -52,6 +60,11 @@
         const data = await fetchData;
         plotter.update(data, svg)
         legendPlotter.update(data, legend)
+        d3.select('#title').text('Movie Sales')
+        d3.select('#description').text('Top 100 Highest Grossing Movies Grouped By Genre')
+
+        d3.selectAll('#dataset-selectors>button').classed('active', false);
+        d3.select('#dataset-selectors>#movies').classed('active', true);
     });
 
     document.getElementById("games").addEventListener("click", async () => {
@@ -60,5 +73,10 @@
         const data = await fetchData;
         plotter.update(data, svg)
         legendPlotter.update(data, legend)
+        d3.select('#title').text('Video Game Sales')
+        d3.select('#description').text('Top 100 Most Sold Video Games Grouped by Platform')
+
+        d3.selectAll('#dataset-selectors>button').classed('active', false);
+        d3.select('#dataset-selectors>#games').classed('active', true);
     });
 })();
